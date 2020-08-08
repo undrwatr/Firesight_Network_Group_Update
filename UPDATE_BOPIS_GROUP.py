2c0402bd32d5c26b4c36c97baff43d33bcee926d
Joe Jenkins <jjenkins@Mantaray.local>
2020-08-08 07:04:15 -0700

    Updated program that pulls in all of the data and the various information to build the json and then upload it to Firesight.

--- UPDATE_BOPIS_GROUP.py
+++ UPDATE_BOPIS_GROUP.py
@@ -7,17 +7,64 @@ import json
 import sys
 import requests
 import cred
+import pyodbc
 
 
-# pull in list of stores from file
+# pull in list of stores from file and format them for the sql query
 stores = open("stores.txt", "r")
 
-#use that list of stores to generate a script to pull the data from the database
+#variables needed for the sql connection
+server = cred.server
+sqlserver = cred.sql_host
+username = cred.sql_username
+password = cred.sql_password
+database = cred.sql_database
 
-#use the data from the database to generate the list of literals down below for upload
+#makes the connection to the sql server
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+sqlserver+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+VLAN2LAN = {}
+LITERALS = []
+OBJECTS = []
+VLAN2LAN["id"] = "005056A2-7618-0ed3-0000-515396143619"
+VLAN2LAN["name"] = "BOPIS_TEST"
+VLAN2LAN["type"] = "NetworkGroup"
+VLAN2LAN["objects"] = OBJECTS
+for storenum in stores:
+    stripped = (storenum.rstrip())
+    cursor.execute("select [VID2NET] from tblDSlip where [Store #] = ?", stripped)
+    LITERALS.append({'type' : "Network", 'value' : (str(cursor.fetchone()[0]) + '/28')})
+    #LITERALS.append({'value' : (str(cursor.fetchone()[0]) + '/28')})
+
+
+VLAN2LAN["literals"] = LITERALS
+
+# VLAN2LAN = ('{')
+# VLAN2LAN = (VLAN2LAN + '"id": "005056A2-7618-0ed3-0000-515396143619",')
+# VLAN2LAN = (VLAN2LAN + '"name": "BOPIS_TEST",')
+# VLAN2LAN = (VLAN2LAN + '"type": "NetworkGroup",')
+# VLAN2LAN = (VLAN2LAN + '"objects": [],')
+# VLAN2LAN = (VLAN2LAN + '"literals"' + ': ' + '[')
+
+# for storenum in stores:
+#     stripped = (storenum.rstrip())
+#     cursor.execute("select [VID2NET] from tblDSlip where [Store #] = ?", stripped)
+#     VLAN2LAN = (VLAN2LAN + ' {')
+#     VLAN2LAN = (VLAN2LAN + '"type": "Network", ')
+#     VLAN2LAN = (VLAN2LAN + '"value": ' + '"' + str(cursor.fetchone()[0]) + '/28' + '"')
+#     VLAN2LAN = (VLAN2LAN + '},')
+
+# VLAN2LAN = (VLAN2LAN + '{')
+# VLAN2LAN = (VLAN2LAN + '"type": "Network",')
+# VLAN2LAN = (VLAN2LAN + '"value": ' + '"1.1.1.0/24"')
+# VLAN2LAN = (VLAN2LAN + '}]}')
+
+
+stores.close()
+
 
 #This is the part of the code from the API EXPLORER that has been imported 
-server = cred.server
  
 username = cred.username
 if len(sys.argv) > 1:
@@ -57,24 +104,20 @@ if (url[-1] == '/'):
  
 # PUT OPERATION
  
-
- 
-
- 
-# The literals data is generated from the sql query and then generates this to be uploaded
-put_data = {
-  "id": "005056A2-7618-0ed3-0000-515396143619",
-  "name": "BOPIS_TEST",
-  "type": "NetworkGroup",
-  "objects": [
- ],
-  "literals": [
- #data to be built from above
-  ]
-}
+# The literals data is generated from the sql query and then generates this to be uploaded, this data is generated higher up in the program.
+# put_data = {
+#   "id": "005056A2-7618-0ed3-0000-515396143619",
+#   "name": "BOPIS_TEST",
+#   "type": "NetworkGroup",
+#   "objects": [
+#  ],
+#   "literals": [
+#  #data to be built from above
+#   ]
+# }
 try:
     # REST call with SSL verification turned off:
-    r = requests.put(url, data=json.dumps(put_data), headers=headers, verify=False)
+    r = requests.put(url, data=json.dumps(VLAN2LAN), headers=headers, verify=False)
     # REST call with SSL verification turned on:
     # r = requests.put(url, data=json.dumps(put_data), headers=headers, verify='/path/to/ssl_certificate')
     status_code = r.status_code
